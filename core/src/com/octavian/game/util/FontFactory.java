3bc785ca09c85b10b16c20add20730510dccbeda
boctavian96 <boctavian96@yahoo.com>
2019-04-09 21:27:24 +0300

    Font factory created.

---
+++ core/src/com/octavian/game/util/FontFactory.java
@@ -0,0 +1,45 @@
+package com.octavian.game.util;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+/**
+ * Created by octavian on 4/9/19.
+ */
+
+public final class FontFactory {
+    public static final String FONT_PIXELED = "fonts/Pixeled.ttf";
+    public static final String FONT_BEBAS_NEUE = "fonts/BebasNeue.otf";
+
+    private static FontFactory instance;
+
+    private BitmapFont font;
+
+    private FontFactory(){super();}
+
+    public static synchronized FontFactory getInstance(){
+        if(instance == null){
+            instance = new FontFactory();
+        }
+        return instance;
+    }
+
+    public BitmapFont generateFont(String fontName, int size, Color color){
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(fontName));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        parameter.size = size;
+        parameter.color = color;
+
+        font = generator.generateFont(parameter);
+        generator.dispose();
+
+        return font;
+    }
+
+    public void dispose(){
+        font.dispose();
+    }
+}
